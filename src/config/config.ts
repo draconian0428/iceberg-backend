ccaa1f1f24edbb534ac7bee6acce65aef8ca0b29
webdragon0125 <jakus.superdev@gmail.com>
2023-05-04 07:56:10 -0700

    Update MongoDB URL

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 export default {
   PORT: process.env.PORT ?? 8080,
-  MONGO_URI: process.env.MONGO_URI ?? "mongodb+srv://robin:robin@cluster0.zksqaj9.mongodb.net/?retryWrites=true&w=majority"
+  MONGO_URI: process.env.MONGO_URI ?? "mongodb+srv://iceberguser:accesstoassessment@icebergassess.rbjxgi4.mongodb.net/iceberglabsassess"
  }
